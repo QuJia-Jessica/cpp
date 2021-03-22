e2958a5149946709f00ad1cec68d6a63dae95e8d
QuJia-Jessica <66230627+QuJia-Jessica@users.noreply.github.com>
2021-03-22 10:32:39 +0800

    Create Exe_6.12.cpp

---
+++ Exe_6.12.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+void swap(int& v1, int& v2){
+    int tmp = v1;
+    v1 = v2;
+    v2 = tmp;
+}
+
+int main(){
+    int v1, v2;
+    std::cout << "Integers before swap: ";
+    std::cin >> v1 >> v2;
+    std::cout << "Integers after swap: ";
+    swap(v1, v2);
+    std::cout << v1 << " " << v2 << std::endl;
+    return 0;
+}
