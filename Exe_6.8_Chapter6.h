edf59a9e195433dded1b8ddb451f0bf98fa6a346
QuJia-Jessica <66230627+QuJia-Jessica@users.noreply.github.com>
2021-03-22 09:33:53 +0800

    Create Exe_6.8_Chapter6.h

---
+++ Exe_6.8_Chapter6.h
@@ -0,0 +1,10 @@
+#ifndef CLIONPROJECTS_CHAPTER6_H
+#define CLIONPROJECTS_CHAPTER6_H
+
+#endif //CLIONPROJECTS_CHAPTER6_H
+
+int fact(int val);
+int func();
+
+template<typename T>
+T abs(T i){ return i >= 0 ? i : -i;}
